797ef4ffe871974b2b56b326b8c6fbcab943a4bc
Marcus <54941923+rodercode@users.noreply.github.com>
2023-09-10 10:52:09 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <baselineSignalWifi1Bar />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import baselineSignalWifi1Bar from '~icons/ic/baseline-signal-wifi-1-bar';
+
+export default defineComponent({
+  components:{
+    baselineSignalWifi1Bar
+  },
+  name: "HomeView",
+  data() {
+    return {};
+  },
+});
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#00668A",
+        "secondary-color": "#004E71",
+        "gray-color": "#495057",
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

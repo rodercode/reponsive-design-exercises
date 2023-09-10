66ebbc32dc2aabe3ab0b955dcbf03f380db540cd
rodercode <rodersnow@gmail.com>
2023-09-10 12:52:01 +0200

    handle click event for btn menu and create a btn menu component

---
+++ src/components/BtnMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="toggleMenu">
+    <IconMenu class="w-8 h-8" />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+// IMPORT ICONS
+import IconMenu from "~icons/heroicons/bars-3";
+
+export default defineComponent({
+  components: {
+    IconMenu,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    toggleMenu() {
+      console.log("toggle menu");
+    },
+  },
+});
+</script>
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,25 +1,23 @@
 <template>
   <header class="flex justify-between py-6">
     <HomeLink />
-    <IconMenu class="w-8 h-8" />
+    <BtnMenu />
   </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
-// IMPORT ICONS
-import IconMenu from "~icons/heroicons/bars-3";
-
 // IMPORT COMPONENTS
 import HomeLink from "./main-header/HomeLink.vue";
 import BaseLinkButton from "./base/BaseLinkButton.vue";
+import BtnMenu from "./BtnMenu.vue";
 
 export default defineComponent({
   components: {
     HomeLink,
     BaseLinkButton,
-    IconMenu,
+    BtnMenu,
   },
   data() {
     return {};
